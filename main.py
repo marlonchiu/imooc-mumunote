c7bb80de3cc44a58a5e838e3c0d9607357e7268a
marlonchiu <Knight1898@outlook.com>
2025-03-21 11:39:34 +0800

    3-7 Flask 实现 Session 开发

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # 导入 Flask
-from flask import Flask, make_response, request
+from flask import Flask, make_response, request, session
 from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
+
+# 设置secret key用于session加密
+app.config['SECRET_KEY'] = os.urandom(24)  # 生成随机密钥
 
 @app.route('/')
 def hello_world():
     return 'Hello World！'
 
 # 设置cookie
-@app.route('/cookie', methods=['POST', 'GET'])
-def cookie():
+@app.route('/add_cookie', methods=['POST', 'GET'])
+def add_cookie():
     response = make_response('设置cookie成功')
     # response.set_cookie('username', 'admin',max_age=10) # 设置cookie的过期时间 10s
 
@@ -49,6 +53,38 @@ def del_cookie():
     for key, value in cookies_dict.items():
         response.delete_cookie(key)
     return response
+
+# 操作session
+# 设置session
+@app.route('/add_session')
+def add_session():
+    session['username'] = 'marlon'
+    session['nickname'] = 'zxh'
+    session['role'] = 'admin'
+    return '设置session成功'
+
+# 获取session (要获取必须先设置)
+@app.route('/get_session')
+def get_session():
+    print(session)
+    username = session.get('username')
+    nickname = session.get('nickname')
+    role = session.get('role')
+    print('username: %s, nickname: %s, role: %s' % (username, nickname, role))
+    return '获取session成功 >>> ' + 'username: %s, nickname: %s, role: %s' % (username, nickname, role)
+
+# 删除session
+@app.route('/del_session')
+def del_session():
+    # 清除单个
+    # session.pop('username')
+    # session.pop('nickname')
+
+    # 清除所有
+    session.clear()
+    # session.pop('role')
+    print(session)
+    return '删除session成功'
 
 if __name__ == '__main__':
     # app.run()
