7b4b4c4e7cc3e77901b87dc2ccb63d4f081e348c
marlonchiu <Knight1898@outlook.com>
2025-03-25 10:18:04 +0800

    10-7 首页文章数据填充（二）

--- app/config/config.py
+++ app/config/config.py
@@ -3,6 +3,8 @@ class Config(object):
     db_url = "mysql+pymysql://root:123456@127.0.0.1:3306/mumushouji"
     # 前端页面显示的条数
     page_count = 10
+    # 配置一下文章图片的存储路径
+    article_header_image_path = "/images/article/header/"
 
 # 测试环境
 class TestConfig(Config):
--- controller/index.py
+++ controller/index.py
@@ -7,6 +7,18 @@ from model.article import Article
 
 index = Blueprint("index",__name__)
 
+
+# 对应前端显示分类的字典
+label_types = {
+    "recommend": {"name": "推荐", "selected": "selected"},
+    "auto_test": {"name": "自动化测试", "selected": "no-selected"},
+    "python": {"name": "Python", "selected": "no-selected"},
+    "java": {"name": "Java", "selected": "no-selected"},
+    "function_test": { "name": "功能测试", "selected": "no-selected"},
+    "perf_test": {"name": "性能测试", "selected": "no-selected"},
+    "funny": {"name": "幽默段子", "selected": "no-selected"},
+}
+
 @index.route("/")
 def home():
     # 获取当前到底是第几页
@@ -24,4 +36,19 @@ def home():
     article = Article()
     db_result = article.find_article(page, article_type)
 
-    return render_template("index.html", result=db_result)
+    for article, nickname in db_result:
+      # 分类内容显示的转换
+      article.label = label_types.get(article.label_name).get("name")
+
+      # 日期的显示
+      article.create_time = str(article.create_time.month) + '.' + str(article.create_time.day)
+
+      # 图片路径的处理  "/images/article/header/"
+      article.article_image = config[env].article_header_image_path + str(article.article_image)
+
+      # 文章标签格式的修改
+      article.article_tag = article.article_tag.replace(","," · ")
+
+    return render_template("index.html",
+                          result=db_result,
+                          label_types=label_types)
